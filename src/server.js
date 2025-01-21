520dec176a8b10ee3d6d35843def9b04b9888bf1
Paul_Delamare <paulodela2006@gmail.com>
2025-01-21 11:32:10 +0100

    Commenter le code de connexion à MongoDB

--- src/server.js
+++ src/server.js
@@ -37,10 +37,10 @@ app.listen(process.env.PORT, () => {
 });
 
 // Database
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-        console.log('Connected to MongoDB');
-    })
-    .catch((error) => {
-        console.error('Error connecting to MongoDB:', error);
-    });
+// mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+//     .then(() => {
+//         console.log('Connected to MongoDB');
+//     })
+//     .catch((error) => {
+//         console.error('Error connecting to MongoDB:', error);
+//     });
