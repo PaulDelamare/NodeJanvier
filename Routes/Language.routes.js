fd9f0fceb593d17470aef1423b52145f149b44c1
Paul_Delamare <paulodela2006@gmail.com>
2025-01-28 16:07:40 +0100

    Ajout de la pagination pour la récupération des langues dans les routes

--- Routes/Language.routes.js
+++ Routes/Language.routes.js
@@ -7,5 +7,6 @@ router.get('/language', LanguageController.findAll);
 router.get('/language/:id', LanguageController.findOne);
 router.delete('/language/:id', LanguageController.delete);
 router.put('/language/:id', LanguageController.update);
+router.get('/language', LanguageController.getPagination);
 
 module.exports = router;
